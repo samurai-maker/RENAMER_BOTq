d5c54f96c3c22ccb1c6210790f30dce34ea9de5a
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-04-29 08:59:56 -0700

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -1,5 +1,25 @@
 from pyrogram import Client, filters
-from helper.database import find, delthumb, addthumb
+from helper.database import find, delthumb, addthumb,insert
+from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+
+@Client.on_message(filters.private & filters.command(['help']))
+async def  help(client,message):
+		insert(int(message.chat.id))
+		await message.reply_text(text =f"""
+	__/viewthumb__ : **View current thumbnil** 
+	__/delthumb__  : **Delete thumbnil**
+	
+	**Dev contact @slogan_98**
+	""",reply_to_message_id = message.message_id,
+	 reply_markup=InlineKeyboardMarkup(
+	 [
+                [
+                    InlineKeyboardButton("мσνιє яєqυєѕт gяσυρ 📲", url="https://t.me/WORLD_WIDE_MOVIES")],
+                    [InlineKeyboardButton("🏐ᵘʳˡ ᵈᵒʷⁿˡᵒᵃᵈᵉʳ", url="https://t.me/youtube_instagram_downloaderbot")
+                ]
+            ]))
+
+
 
 @Client.on_message(filters.private & filters.command(['viewthumb']))
 async def viewthumb(client,message):
@@ -7,7 +27,7 @@ async def viewthumb(client,message):
 		if thumb :
 			await client.send_photo(message.chat.id,photo =f"{thumb}")
 		else:
-			await message.reply_text("**You dont have any custom Thumbnail**")
+			await message.reply_text("**You dont have any custom Thumbnail.send a new  thumbnil**")
 	
 	
 @Client.on_message(filters.private & filters.command(['delthumb']))
